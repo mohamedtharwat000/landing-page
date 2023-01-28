b8dccd7411ac8f61c46c9c596cf8546210877381
mohamedtharwat000 <81764836+mohamedtharwat000@users.noreply.github.com>
2023-01-28 02:17:28 +0200

    update js functions

--- js/app.js
+++ js/app.js
@@ -1,104 +1,97 @@
-
 /*
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
-*/
-
+ */
 
 //Define Global Variables
-var navBarList = document.getElementById('navbar__list');
-var allSections = document.querySelectorAll('section');
-var allSectionsNumber = allSections.length;
+var navBarList = document.getElementById("navbar__list");
+var allSections = document.querySelectorAll("section");
 var documentFragment = document.createDocumentFragment();
 //End Global Variables
 
-
 //Start Helper Functions
 
-
 //End Helper Functions
-
 
 /********** Begin Main Functions **********/
 
 // build the nav
-window.addEventListener('load', buildNav())
+window.addEventListener("load", buildNav());
 
 function buildNav() {
-
-    for(i = 0; i < allSectionsNumber; i = i + 1){
-        let sectionList = document.createElement("li");
-    	let sectionName = allSections[i].getAttribute("data-nav");
-    	let sectionId = allSections[i].getAttribute("id");
-        sectionList.innerHTML = `<a href="#${sectionId}">${sectionName}</a>`;
-        documentFragment.appendChild(sectionList)
-    };
-    navBarList.appendChild(documentFragment);
-};
-
+  for (i = 0; i < allSections.length; i = i + 1) {
+    let sectionList = document.createElement("li");
+    let sectionName = allSections[i].getAttribute("data-nav");
+    let sectionId = allSections[i].getAttribute("id");
+    sectionList.innerHTML = `<a href="#${sectionId}">${sectionName}</a>`;
+    documentFragment.appendChild(sectionList);
+  }
+  navBarList.appendChild(documentFragment);
+}
 
 // Add class 'active' to section when near top of viewport
 
-// window.onscroll = function() {
+// window.onscroll = function () {
+//   allSections.forEach((section) => {
+//     if (
+//       section.getBoundingClientRect().top >= -400 &&
+//       section.getBoundingClientRect().top <= 150
+//     ) {
+//       section.classList.add("your-active-class");
+//     } else {
+//       section.classList.remove("your-active-class");
+//     }
+//   });
+// };
 
-// 	allSections.forEach(section => {
-
-//         let activeLink = document.querySelectorAll("nav a").forEach;
-
-// 	    if(section.getBoundingClientRect().top >= -400 && section.getBoundingClientRect().top <= 150){
-
-//             section.classList.add("your-active-class");
-//             activeLink.classList.add("active-link");
-
-//         }
-
-//         else{
-//             section.classList.remove("your-active-class");
-//             activeLink.classList.remove("active-link");
-//         }
-    
-// 	})
-// }
-
-window.onscroll = function() {
-
-    allSections.forEach(section => {
-    
-        if(section.getBoundingClientRect().top >= -400 && section.getBoundingClientRect().top <= 150)
-            section.classList.add("your-active-class");
-        else
-            section.classList.remove("your-active-class");  
-    })}
-
-
-
-
-
+// new function without magic numbers
+window.onscroll = () => {
+  allSections.forEach((section) => {
+    if (
+      scrollY > section.offsetTop - screen.height / 2 &&
+      scrollY < section.offsetTop + section.offsetHeight - screen.height / 2
+    ) {
+      if (!section.classList.contains("active-section")) {
+        section.classList.add("active-section");
+      }
+    } else {
+      if (section.classList.contains("active-section")) {
+        section.classList.remove("active-section");
+      }
+    }
+  });
+};
 
 // Scroll to anchor ID using scrollTO event
 
 scrollToSection();
 function scrollToSection() {
+  let navA = document.querySelectorAll("nav a");
 
-    let navA = document.querySelectorAll("nav a");
-
-    navA.forEach(a => {
-        a.addEventListener("click", function(event) {
-            event.preventDefault();
-            document.querySelector(a.getAttribute('href')).scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
-        });
+  navA.forEach((a) => {
+    a.addEventListener("click", function (event) {
+      event.preventDefault();
+      let hash = a.hash;
+      let section = document.querySelector(`${a.hash}`);
+      section.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+        inline: "nearest",
+      });
+      // location.hash = hash;
+      history.replaceState("", "", location.pathname + hash);
     });
+  });
 }
-
 
 /********** End Main Functions **********/
 
 //Begin Events
 
-// Build menu 
+// Build menu
 
 // Scroll to section on link click
 
-// Set sections as active
+// Set sections as active-section
